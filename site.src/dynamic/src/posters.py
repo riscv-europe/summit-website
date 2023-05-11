8792167d03529f87db2e1a26a35163b29068c213
Christian FABRE <christian.fabre1@cea.fr>
2023-05-11 23:59:03 +0200

    Fix: sorted by last name, prety print of country and town.

--- site.src/dynamic/src/posters.py
+++ site.src/dynamic/src/posters.py
@@ -10,13 +10,33 @@ import csv
 
 
 with open('gdocs/authors-info.csv') as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter=',')
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    csv_sorted = sorted(csv_reader, key=lambda row: row['Last Name'].lstrip(" abcdefghijklmnopqrstuvwxyz"))
     line_count = 0
-    for row in csv_reader:
-        if line_count == 0:
-            print(f'Column names are {", ".join(row)}')
-            line_count += 1
-        else:
-            print(f'Author {row[2]} {row[3]} presents {row[10]}.')
-            line_count += 1
-    print(f'Processed {line_count} lines.')
+    for row in csv_sorted:
+        if row['Presentation Type'] == 'Poster':
+            PaperTitle = row["Paper Title"].replace("\n","")
+            print(f'### {row["First Name"]} {row["Last Name"]} -- {PaperTitle}')
+            print(f'')
+
+            # Format the (city, country) string
+            city    = row["Primary Address - City"]
+            country = row["Primary Address - Country"]
+            if city == "" and country == "":
+                location = ""
+            elif city == "":
+                location = " ("+country+")."
+            elif country == "":
+                location = " ("+city+")."
+            else:
+                location = " ("+city+", "+country+")."
+
+            print(f'{row["First Name"]} {row["Last Name"]} -- {row["Organization"]}{location}')
+            if row['Final abstract'] != "":
+                print(f'\n**Summary**\n')
+                print(f'{row["Final abstract"]}')
+            if row['Bio'] != "":
+                print(f'\n*Bio*\n')
+                print(f'{row["Bio"]}')
+            print(f'')
+            print(f'')
