2ef446097e72c5c688fede9468a66c28077258ed
Christian FABRE <christian.fabre1@cea.fr>
2023-05-12 23:53:53 +0200

    Fix: process location, days, and remaining of the authors.

--- site.src/dynamic/src/posters.py
+++ site.src/dynamic/src/posters.py
@@ -1,20 +1,16 @@
 #!/usr/bin/python3
 
-# Columns are: ID, Title, First Name, Last Name, Position,
-# Organization, Primary Address - City, Primary Address - Country,
-# Primary Email, Additional Email, Paper Title, Paper Number,
-# Presentation Status, Presentation Type, Theme, Presenting Author
-# Names, Registered, Bio, Final abstract
+# ID,Title,First Name,Last Name,Position,Organization,Primary Address
+# - City,Primary Address - Country,Primary Email,Additional
+# Email,Paper Title,Paper Number,Presentation Status,Presentation
+# Type,Theme,Presenting Author Names,Rest of the
+# authors,Registered,Bio,Final abstract,Day (Poster)
 
 import csv
 
-
-with open('gdocs/authors-info.csv') as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
-    csv_sorted = sorted(csv_reader, key=lambda row: row['Last Name'].lstrip(" abcdefghijklmnopqrstuvwxyz"))
-    line_count = 0
+def posters_of_day(day):
     for row in csv_sorted:
-        if row['Presentation Type'] == 'Poster':
+        if row['Presentation Type'] == 'Poster' and row['Day (Poster)'] == day:
             PaperTitle = row["Paper Title"].replace("\n","")
             print(f'### {row["First Name"]} {row["Last Name"]} -- {PaperTitle}')
             print(f'')
@@ -25,13 +21,25 @@ with open('gdocs/authors-info.csv') as csv_file:
             if city == "" and country == "":
                 location = ""
             elif city == "":
-                location = " ("+country+")."
+                location = country
             elif country == "":
-                location = " ("+city+")."
+                location = city
             else:
-                location = " ("+city+", "+country+")."
+                location = city+", "+country
 
-            print(f'{row["First Name"]} {row["Last Name"]} -- {row["Organization"]}{location}')
+            if row["Organization"] == "" and location == "":
+                orgaloc = ""
+            elif row["Organization"] == "":
+                orgaloc = " ("+location+")"
+            elif location == "":
+                orgaloc = " ("+row["Organization"]+")"
+            else:
+                orgaloc = " ("+row["Organization"]+", "+location+")"
+
+            print(f'{row["First Name"]} {row["Last Name"]}{orgaloc}')
+            if row['Rest of the authors'] != "":
+                print(f', {row["Rest of the authors"]}.')
+
             print(f'\n**Summary**\n')
             if row['Final abstract'] != "":
                 print(f'{row["Final abstract"]}')
@@ -44,3 +52,27 @@ with open('gdocs/authors-info.csv') as csv_file:
                 print(f'(TBD)')
             print(f'')
             print(f'')
+
+
+with open('gdocs/authors-info.csv') as csv_file:
+    csv_reader = csv.DictReader(csv_file, delimiter=',')
+    csv_sorted = sorted(csv_reader, key=lambda row: row['Last Name'].lstrip(" abcdefghijklmnopqrstuvwxyz"))
+    line_count = 0
+    print(f'')
+    print(f'## Posters on Display Tuesday June 6th')
+    print(f'')
+    print(f'Presenters are expected to be with their poster during the morning break, lunch and afternoon break, as well as during the early evening cocktail on Tuesday 6.')
+    print(f'')
+    posters_of_day('Tue')
+    print(f'')
+    print(f'## Posters on Display Wednesday June 7th')
+    print(f'')
+    print(f'Presenters are expected to be with their poster during the morning break, lunch and afternoon break.')
+    print(f'')
+    posters_of_day('Wed')
+    print(f'')
+    print(f'## Posters on Display Thursday June 8th')
+    print(f'')
+    print(f'Presenters are expected to be with their poster during the morning break, lunch and afternoon break.')
+    print(f'')
+    posters_of_day('Thu')
