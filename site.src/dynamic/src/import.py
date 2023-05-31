ec98a0e34c8beb242bc0210676b7bc65b3ebd4d6
Christian FABRE <christian.fabre1@cea.fr>
2023-05-31 18:42:50 +0200

    Fix: comment code and make it more readable.

--- site.src/dynamic/src/import.py
+++ site.src/dynamic/src/import.py
@@ -13,14 +13,33 @@ def import_file(import_pdf,reposi_pdf):
     print(f'Moving file from "{import_path}" to "{reposi_path}"')
     # shutil.move(import_path,reposi_path)
 
+# We loop over all the PDF files of the abstracts.
 for import_pdf in os.listdir(import_dir):
+
+    # Open the spreadhseet database.
     with open('gdocs/authors-info.csv') as database_file:
         found = False
         database_reader = csv.DictReader(database_file, delimiter=',')
+
+        # We loop over the contribs from the spreadsheet.
         for contrib in database_reader:
-            if import_pdf.replace('.pdf','') == contrib['BaseName']:
-                import_file(import_pdf,contrib['BaseFileName']+"-abstract.pdf")
-                found = True
+            # The current contrib is not the right one. Iterate to the
+            # next one.
+            if not contrib['BaseName'] == import_pdf.replace('.pdf',''):
                 continue
+
+            # This is the expected contribution.
+            found = True
+            if contrib['HasAbstract'] == 'Y':
+                import_file(import_pdf,contrib['BaseFileName']+"-abstract.pdf")
+            else:
+                # This is a new contribution, not yet marked as having
+                # an abstract. Should be fixed in the spreadsheet.
+                print(f'No yet marked as having an abstract: "{import_pdf}"')
+            break
+
+        # We exhausted the spreadsheet and dit not found the contrib
+        # correspondingto the abstract.  Must be missing from the
+        # sheet, or the contrib is not nale properly.
         if not found:
-            print(f'Orphan: "{import_pdf}"')
+            print(f'Not found in spreadsheet: "{import_pdf}"')
